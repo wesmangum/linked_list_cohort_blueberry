7af39eb74ce010f54a7b458f59b5700dca677bc6
wesmangum <wesmangum24@gmail.com>
2014-07-11 09:33:28 -0500

    linked_list_items finished

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,34 @@
 class LinkedList
+  attr_reader :array
+  def initialize(item=nil)
+    if item
+      @array = [item]
+    else
+      @array = []
+    end
 
+    def size
+      @array.length
+    end
+  end
+
+  def push(item)
+    @array.push(item)
+  end
+
+  def get(index)
+    if index < 0 || @array[index] === nil
+      raise IndexError
+    else
+      @array[index]
+    end
+  end
+
+  def last
+    @array[-1]
+  end
+
+  def to_s
+
+  end
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,32 @@
 class LinkedListItem
   include Comparable
+  attr_reader :payload, :next_item
+
+  def initialize(value)
+    @payload = value
+  end
+
+  def next_item=(other_item)
+    if self == other_item
+      raise ArgumentError
+    else
+      @next_item = other_item
+    end
+  end
+
+  def last?
+    if @next_item == nil
+      true
+    else
+      false
+    end
+  end
+
+  def <=>(other_item)
+    self.payload.to_s <=> other_item.payload.to_s
+  end
+
+  def ===(other_item)
+    self.equal? other_item
+  end
 end
